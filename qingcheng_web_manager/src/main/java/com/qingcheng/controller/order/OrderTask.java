43fd0179375b3b553c651af0f180e5321a695dc0
qianye <17352921167@163.com>
2020-03-05 22:04:28 +0800

    echart

--- qingcheng_web_manager/src/main/java/com/qingcheng/controller/order/OrderTask.java
+++ qingcheng_web_manager/src/main/java/com/qingcheng/controller/order/OrderTask.java
@@ -1,6 +1,7 @@
 package com.qingcheng.controller.order;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.qingcheng.service.order.CategoryReportService;
 import com.qingcheng.service.order.OrderService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -9,11 +10,20 @@ import java.util.Date;
 
 @Component
 public class OrderTask {
+//    @Reference
+//    private OrderService orderService;
+//    @Scheduled(cron = "0 0/2 * * * ?")
+//    public void orderTimeOutLogic(){
+//        System.out.println("每两分钟间隔执行一次任务"+ new Date());
+//        orderService.orderTimeOutLogic();
+//    }
+
     @Reference
-    private OrderService orderService;
-    @Scheduled(cron = "0 0/2 * * * ?")
-    public void orderTimeOutLogic(){
-        System.out.println("每两分钟间隔执行一次任务"+ new Date());
-        orderService.orderTimeOutLogic();
+    private CategoryReportService categoryReportService;
+
+    @Scheduled(cron = "0 0 1 * * ?")
+    public void createCategoryReportDate(){
+        System.out.println("生成类目统计数据");
+        categoryReportService.createData();
     }
 }
